db6b92ade479a562463a4a37fc6a7c88691cae0b
蔓荆子 <19744244@qq.com>
2019-05-15 16:21:28 +0800

    add phpredis

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
     <li><a href="phpinfo.php">phpinfo</a></li>
     <li><a href="mysql.php">mysql</a></li>
     <li><a href="redis.php">redis</a></li>
+	<li><a href="/phpRedisAdmin">phpRedisAdmin</a></li>
 </ul>
